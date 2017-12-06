18ace282acd3a7893fc2dbab67a3981e4eb6440e
igor <igor.soldatov@gmail.com>
2017-12-06 15:51:30 +0300

    Scripts for generating commands for stress-testing bmchain

--- start.py
+++ start.py
@@ -37,45 +37,50 @@ def create_content():
         with open(file_name, 'w') as the_file:
             the_file.write('#!/bin/bash\n\n')
             the_file.write('#chmod +x ./' + file_name + '\n\n')
-            the_file.write('./cli_wallet --server-rpc-endpoint="ws://127.0.0.1:9876" <<END\n')
-            the_file.write('unlock "test"\n')
 
-            for idx in range(idx_thread * 1000, (idx_thread * 1000) + 1000):
-                user_idx = str(idx // 10).zfill(3)
-                post_idx = str(idx % 10).zfill(3)
-                author = 'user' + user_idx
-                permlink = author + '-post' + post_idx
-                parent_author = ''
-                parent_permlink = 'bmchain'
-                title = permlink
-                body = permlink
-                json_meta = ''
-                broadcast = 'true'
-                command = 'post_comment "' \
-                          + author + '" "' \
-                          + permlink + '" "' \
-                          + parent_author + '" "' \
-                          + parent_permlink + '" "' \
-                          + title + '" "' \
-                          + body + '" "' \
-                          + json_meta + '" ' \
-                          + broadcast + '\n'
-                the_file.write(command)
-                # vote(string voter, string author, string permlink, int16_t weight, bool broadcast, string comment_bmchain)
-                for idx_v in range(idx_thread * 100, (idx_thread * 100) + 100):
-                    voter = 'user' + str(idx_v).zfill(3)
-                    weight = '100'
+            for iter in range(0, 10):
+                the_file.write('./cli_wallet --server-rpc-endpoint="ws://127.0.0.1:9876" <<END\n')
+                the_file.write('unlock "test"\n')
+
+                begin = idx_thread * 1000 + iter * 100
+                end = (idx_thread * 1000) + iter * 100 + 100
+                for idx in range(begin, end):
+                    user_idx = str(idx // 10).zfill(3)
+                    post_idx = str(idx % 10).zfill(3)
+                    author = 'user' + user_idx
+                    permlink = author + '-post' + post_idx
+                    parent_author = ''
+                    parent_permlink = 'bmchain'
+                    title = permlink
+                    body = permlink
+                    json_meta = ''
                     broadcast = 'true'
-                    comment_bmchain = ''
-                    command_v = 'vote "' \
-                                + voter + '" "' \
-                                + author + '" "' \
-                                + permlink + '" ' \
-                                + weight + ' ' \
-                                + broadcast + ' "' \
-                                + comment_bmchain + '"\n'
-                    the_file.write(command_v)
-            the_file.write('END')
+                    command = 'post_comment "' \
+                              + author + '" "' \
+                              + permlink + '" "' \
+                              + parent_author + '" "' \
+                              + parent_permlink + '" "' \
+                              + title + '" "' \
+                              + body + '" "' \
+                              + json_meta + '" ' \
+                              + broadcast + '\n'
+                    the_file.write(command)
+                    # vote(string voter, string author, string permlink, int16_t weight, bool broadcast, string comment_bmchain)
+                    for idx_v in range(idx_thread * 100, (idx_thread * 100) + 100):
+                        voter = 'user' + str(idx_v).zfill(3)
+                        weight = '100'
+                        broadcast = 'true'
+                        comment_bmchain = ''
+                        command_v = 'vote "' \
+                                    + voter + '" "' \
+                                    + author + '" "' \
+                                    + permlink + '" ' \
+                                    + weight + ' ' \
+                                    + broadcast + ' "' \
+                                    + comment_bmchain + '"\n'
+                        the_file.write(command_v)
+                the_file.write('END\n\n')
+                the_file.write('sleep 3s\n\n')
 
 
 
